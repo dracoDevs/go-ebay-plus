81649a0265474ea3c1d2182a120066467d884c99
dracoDevs <67403295+dracoDevs@users.noreply.github.com>
2025-02-20 16:05:39 -0500

    Update ebay.go

--- ebay.go
+++ ebay.go
@@ -51,6 +51,8 @@ func (e EbayConf) RunCommand(c Command) (EbayResponse, error) {
 		bodyStr := body.String()
 		bodyStr = utils.RemoveEndItemXML(bodyStr)
 		body = bytes.NewBufferString(bodyStr)
+
+		fmt.Println("bodyStr", bodyStr)
 	}
 
 	if e.Logger != nil {
